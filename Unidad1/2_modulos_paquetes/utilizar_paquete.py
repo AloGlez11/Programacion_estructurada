814fb99afd375dc26926095e75448540b9b35892
Alondra Gonzalez Cabral <raquel_3141240115@utd.edu.mx>
2025-07-01 18:39:20 -0600

    Delete Unidad1 directory

--- Unidad1/2_modulos_paquetes/utilizar_paquete.py
+++
@@ -1,9 +0,0 @@
-from paquete1 import modulo
-
-print(modulo.saludar("Sebastian Gonzalez de Leon"))
-
-
-modulo.borrarpantalla()
-nom,tel=modulo.SolicitarDatos2()
-print(f"\n\t.::Agenda Telefonica::.\n\t\tNombre: {nom}\n\t\tTelefono:{tel}")
-modulo.espereTecla()
